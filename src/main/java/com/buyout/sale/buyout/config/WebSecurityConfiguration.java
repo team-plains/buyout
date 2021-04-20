14bcc715d9b4b4f8367a596d372ea5d0e8f340c5
Stephen Webber <offgridauthor@gmail.com>
2021-04-20 10:14:14 -0700

    minor styling and access

--- src/main/java/com/buyout/sale/buyout/config/WebSecurityConfiguration.java
+++ src/main/java/com/buyout/sale/buyout/config/WebSecurityConfiguration.java
@@ -48,7 +48,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
                 //authorization routes and settings
                 .authorizeRequests()
 //                .antMatchers("/").permitAll()
-                .antMatchers("/signup","/login","/","/profile","/duplication").permitAll()
+                .antMatchers("/signup","/login","/","/profile","/duplication", "/css/**").permitAll()
                 .anyRequest().authenticated()
 
 
