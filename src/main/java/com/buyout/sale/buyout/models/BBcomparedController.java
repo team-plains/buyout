33657c197c83e244d65b190e46785344559cab53
Cristian Robles <formymoment12@gmail.com>
2021-04-21 18:18:43 -0500

    Maybe fixed?

--- src/main/java/com/buyout/sale/buyout/models/BBcomparedController.java
+++ src/main/java/com/buyout/sale/buyout/models/BBcomparedController.java
@@ -4,43 +4,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 
     public class BBcomparedController {
-        BufferedReader reader;
-        String line;
-        StringBuffer responseContent = new StringBuffer();
-        try {
-        String bbURL = ("https://api.bestbuy.com/v1/products((search=Samsung&search=tv)&(categoryPath.id=abcat0101000))                                                                ?apiKey=01A8DwW36mBlILSxadcGJu5r&sort=regularPrice.asc&show=regularPrice,name,image&pageSize=3&format=json");
-        URL url = new URL(bbURL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        }
-        // Request setup
-        connection.setRequestMethod("GET");
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        int status = connection.getResponseCode();
-        if (status > 299) {
-        reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
 
-        while ((line = reader.readLine()) != null) {
-        responseContent.append(line);
-        }
-        reader.close();
-        } else {
-        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        while ((line = reader.readLine()) != null) {
-        responseContent.append(line);
-        }
-        reader.close();
-        }
-        System.out.println(responseContent.toString());
-        } catch (MalformedURLException e) {
-        e.printStackTrace();
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
+        public static void getApiInformation() throws IOException {
+            BufferedReader reader;
+            String line;
+            StringBuffer responseContent = new StringBuffer();
+            HttpURLConnection connection;
+            try {
+                String bbURL = ("https://api.bestbuy.com/v1/products((search=Samsung&search=tv)&(categoryPath.id=abcat0101000))?apiKey=01A8DwW36mBlILSxadcGJu5r&sort=regularPrice.asc&show=regularPrice,name,image&pageSize=3&format=json");
+                URL url = new URL(bbURL);
+                connection = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                   e.printStackTrace();
+                     }
+            // Request setup
+
+            connection.setRequestMethod("GET");
+
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int status = 0;
+            try {
+                status = connection.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+
         }
     }
-}
