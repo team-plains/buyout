e089b2eba40fbf3b8867f082553a879cd930da52
Stephen Webber <offgridauthor@gmail.com>
2021-04-22 17:48:27 -0700

    stripe working

--- src/main/java/com/buyout/sale/buyout/controllers/ChargeRequest.java
+++ src/main/java/com/buyout/sale/buyout/controllers/ChargeRequest.java
@@ -13,6 +13,46 @@ public class ChargeRequest {
     private Currency currency;
     private String stripeEmail;
     private String stripeToken;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getStripeEmail() {
+        return stripeEmail;
+    }
+
+    public void setStripeEmail(String stripeEmail) {
+        this.stripeEmail = stripeEmail;
+    }
+
+    public String getStripeToken() {
+        return stripeToken;
+    }
+
+    public void setStripeToken(String stripeToken) {
+        this.stripeToken = stripeToken;
+    }
 }
 
 
